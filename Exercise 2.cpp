f6a53b82699b2b5847d0827b193c6439464d6748
CHARLESYEXELPALOMARES <qcyapalomares@tip.edu.ph>
2024-09-09 05:14:59 +0000

    Add files via upload

---
+++ Exercise 2.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int x=4,y=2;
+    int sum,difference,product,remainder;
+    float quotient;
+    
+	cout << "Input x: ";
+    cin >> x;
+    cout << "Input y: ";
+    cin >> y;
+   
+    sum = x+y;
+    difference = (x - y);
+    product = (x * y);
+    quotient = (x / y); 
+    remainder = (x & y);
+    
+    cout << "Sum: " << sum << endl;
+    cout << "Difference: " << difference << endl;
+    cout << "Product: " << product << endl;
+    cout << "Quotient: " << quotient << endl;
+    cout << "Remainder: " << remainder << endl;
+    
+    return 0;
+}
