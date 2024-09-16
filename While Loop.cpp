3cac785e58cd6bd5890ec79c9282f78ab2896356
CHARLESYEXELPALOMARES <qcyapalomares@tip.edu.ph>
2024-09-16 03:45:21 +0000

    Add files via upload

---
+++ While Loop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i =0;
+    
+    cout<<"Output:"<<endl;
+	while( i < 11)
+	{
+	    cout<<"Hello "<<i<<endl;
+	     i=i+1;	
+	}
+    return 0;
+ 
+}
+
