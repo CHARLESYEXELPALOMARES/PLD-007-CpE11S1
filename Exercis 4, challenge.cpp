adcdc0a886025afa2468923920594cfa51c684c0
CHARLESYEXELPALOMARES <qcyapalomares@tip.edu.ph>
2024-09-16 04:34:26 +0000

    Add files via upload

---
+++ Exercis 4, challenge.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=4;
+    int j=9;
+    
+	cout<<"C++ Program Input displays Integer less than 10 but more than 3 :"<<endl;
+	for(;i<=j;i++)
+	{
+	    cout<<i<<endl;
+	     
+	}
+    return 0;
+ 
+}
