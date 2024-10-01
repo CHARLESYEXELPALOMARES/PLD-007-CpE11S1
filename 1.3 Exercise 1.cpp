dd96d0877802d5b8267028bb10ef8e43ecc75f37
CHARLESYEXELPALOMARES <qcyapalomares@tip.edu.ph>
2024-10-01 15:54:21 +0000

    Add files via upload

---
+++ 1.3 Exercise 1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    
+    printf("Name: Charles Yexel A. Palomares\n");
+    printf("Course: Programming Logic and Design\n");
+    printf("Section: CPE11S1\n");
+
+    return 0;
+}
