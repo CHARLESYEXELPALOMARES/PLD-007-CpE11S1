132b963336a05eaecc4123a1b997699c275a1791
CHARLESYEXELPALOMARES <qcyapalomares@tip.edu.ph>
2024-10-02 04:54:28 +0000

    Add files via upload

---
+++ Practical lab.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i =1;
+    
+    cout<<"Output:"<<endl;
+	while( i < 6 )
+	{
+	    cout<<"The numbers is 1 to 5 = "<<i<<endl;
+	     i=i+1;	
+	}
+    return 0;
+ 
+}
+
